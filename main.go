721b847bc1bec843600383d42f2ddde5351d7a0a
Seyf <seyfettin.baskara@gmail.com>
2022-11-22 14:47:45 +0100

    add initial setup

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"github.com/seyfBaskara/go-project-restfull/initializers"
+	"github.com/seyfBaskara/go-project-restfull/models"
+)
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("? Could not load environment variables", err)
+	}
+
+	initializers.ConnectDB(&config)
+}
+
 
 func main (){
-	fmt.Println("github test")
+	initializers.DB.AutoMigrate(&models.Post{})
+	fmt.Println("? Migration complete")
 }
