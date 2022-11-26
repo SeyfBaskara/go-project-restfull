faabda0aafe208074c3a2287a932737440918c50
Seyf <seyfettin.baskara@gmail.com>
2022-11-26 15:05:44 +0100

    add get update and delete endpoints

--- controllers/post.controller.go
+++ controllers/post.controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -47,3 +48,73 @@ func (pc *PostController) CreatePost(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, gin.H{"status": "success", "data": newPost})
 }
 
+func (pc *PostController) FindPosts(ctx *gin.Context) {
+	var page = ctx.DefaultQuery("page", "1")
+	var limit = ctx.DefaultQuery("limit", "10")
+
+	intPage, _ := strconv.Atoi(page)
+	intLimit, _ := strconv.Atoi(limit)
+	offset := (intPage - 1) * intLimit
+
+	var posts []models.Post
+	results := pc.DB.Limit(intLimit).Offset(offset).Find(&posts)
+	if results.Error != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": results.Error})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "results": len(posts), "data": posts})
+}
+
+func (pc *PostController) FindPostById(ctx *gin.Context) {
+	postId := ctx.Param("postId")
+
+	var post models.Post
+	result := pc.DB.First(&post, "id = ?", postId)
+	if result.Error != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"status": "fail", "message": "No post with that title exists"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "data": post})
+}
+
+func (pc *PostController) UpdatePost(ctx *gin.Context) {
+	postId := ctx.Param("postId")
+
+	var payload *models.UpdatePost
+	if err := ctx.ShouldBindJSON(&payload); err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+	var updatedPost models.Post
+	result := pc.DB.First(&updatedPost, "id = ?", postId)
+	if result.Error != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"status": "fail", "message": "No post with that title exists"})
+		return
+	}
+	now := time.Now()
+	postToUpdate := models.Post{
+		Title:     payload.Title,
+		Content:   payload.Content,
+		CreatedAt: updatedPost.CreatedAt,
+		UpdatedAt: now,
+	}
+
+	pc.DB.Model(&updatedPost).Updates(postToUpdate)
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "data": updatedPost})
+}
+
+func (pc *PostController) DeletePost(ctx *gin.Context) {
+	postId := ctx.Param("postId")
+
+	result := pc.DB.Delete(&models.Post{}, "id = ?", postId)
+
+	if result.Error != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"status": "fail", "message": "No post with that title exists"})
+		return
+	}
+
+	ctx.JSON(http.StatusNoContent, nil)
+}
--- routes/post.routes.go
+++ routes/post.routes.go
@@ -17,4 +17,9 @@ func (pc *PostRouteController) PostRoute(rg *gin.RouterGroup) {
 
 	router := rg.Group("posts")
 	router.POST("/", pc.postController.CreatePost)
+	router.GET("/", pc.postController.FindPosts)
+	router.GET("/:postId", pc.postController.FindPostById)
+	router.PUT("/:postId", pc.postController.UpdatePost)
+	router.DELETE("/:postId", pc.postController.DeletePost)
+
 }
